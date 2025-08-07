b4a0a9840de88c07e73b4c22db456bf03e0bb517
卢江博 <3082738259@qq.com>
2025-08-07 14:14:16 +0800

    feat(share-service): 添加分享服务模块
    - 新增 share-service子模块
    - 添加 RabbitMQ、Redis、Feign、Eureka、Config 等依赖
    - 创建 ShareServiceApplication 启动类- 在根 pom.xml 中添加 share-service 模块引用

---
+++ share-service/src/main/java/org/doubao/share/service/ShareServiceApplication.java
@@ -0,0 +1,13 @@
+package org.doubao.share.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareServiceApplication.class, args);
+	}
+
+}
