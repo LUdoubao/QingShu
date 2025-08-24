980e2a7d2c0f777222fe1967468ce86713d76aa1
卢江博 <3082738259@qq.com>
2025-08-24 16:10:39 +0800

    chore: 删除订单服务相关代码
    - 移除订单服务的实体类、DTO、Mapper、Service、Controller等代码
    - 删除订单服务的配置文件和启动类- 移除与支付服务相关的Feign客户端
    - 删除订单服务在消息队列中的相关代码

--- payment-service/src/main/java/org/doubao/payment/service/service/PaymentService.java
+++
@@ -1,7 +0,0 @@
-package org.doubao.payment.service.service;
-
-import org.doubao.payment.service.dto.PaymentRecordDto;
-
-public interface PaymentService {
-	void pay(PaymentRecordDto paymentRecord);
-}
--- product-service/src/main/java/org/doubao/product/server/mapper/ProductMapper.java
+++
@@ -1,9 +0,0 @@
-package org.doubao.product.server.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.doubao.mall.common.entity.Product;
-
-@Mapper
-public interface ProductMapper extends BaseMapper<Product> {
-}
