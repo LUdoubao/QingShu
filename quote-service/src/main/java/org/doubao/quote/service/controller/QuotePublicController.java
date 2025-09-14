df2f25fe785bf800cf0c973f45d129a4166a85cf
卢江博 <3082738259@qq.com>
2025-09-14 15:25:26 +0800

    refactor(quote-service):脱敏 用户信息
    - 在 QuotePublicController 的 detail 方法中，对返回的 QuoteVo 对象中的 UserInfo 进行脱敏处理
    - 清除了用户信息中的敏感字段，如邮箱、用户名、ID 等
    - 优化了返回结果的处理逻辑，提高了数据安全性

--- quote-service/src/main/java/org/doubao/quote/service/controller/QuotePublicController.java
+++ quote-service/src/main/java/org/doubao/quote/service/controller/QuotePublicController.java
@@ -2,6 +2,7 @@ package org.doubao.quote.service.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.doubao.mall.common.entity.Result;
+import org.doubao.mall.common.entity.UserInfo;
 import org.doubao.quote.service.dto.PageDto;
 import org.doubao.quote.service.dto.QuoteDTO;
 import org.doubao.quote.service.dto.QuoteUpdateDto;
@@ -22,7 +23,21 @@ public class QuotePublicController {
 	private QuoteService quoteService;
 	@GetMapping("/detail/{id}")
 	public Result<QuoteVo> detail(@PathVariable Long id) {
-		return quoteService.publicGetDetailById(id);
+		QuoteVo data = quoteService.publicGetDetailById(id).getData();
+		if (data != null && data.getUserInfo() != null) {
+			UserInfo userInfo = data.getUserInfo();
+			// 脱敏
+			userInfo.setEmail(null);
+			userInfo.setUsername(null);
+			userInfo.setId(null);
+			userInfo.setSignature(null);
+			userInfo.setBgUrl(null);
+			userInfo.setCreatedTime(null);
+			userInfo.setToken(null);
+			userInfo.setRole(null);
+			userInfo.setFollow(false);
+		}
+		return Result.success(data);
 	}
 }
 
